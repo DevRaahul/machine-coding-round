7710b2763841c3f28524717a8aae2661280fe342
Rahul <DevRaahul>
2025-01-18 00:32:08 +0530

    typescript lint fix

--- src/components/questions/MemoryBox/MemoryBoxGame.tsx
+++ src/components/questions/MemoryBox/MemoryBoxGame.tsx
@@ -13,7 +13,7 @@ const MemoryBoxGame: React.FC = () => {
     }
   }, []);
 
-  const setDataCount = () => {
+  const setDataCount = (): void => {
     boxMockData.forEach((dt) => {
       if (dt.isVisible) {
         counterRef.current++;
@@ -21,7 +21,7 @@ const MemoryBoxGame: React.FC = () => {
     });
   };
 
-  const reset = () => {
+  const reset = (): void => {
     const cloneData = [...boxData];
     // cloneData.sort((a, b) => b.order - a.order); to reset box in LIFO fashion
     cloneData.sort((a, b) => a.order - b.order); // to reset box in FIFO fashion
@@ -42,7 +42,7 @@ const MemoryBoxGame: React.FC = () => {
     isResetCalled.current = false;
   };
 
-  const boxClicked = (id: number) => {
+  const boxClicked = (id: number): void => {
     if (isResetCalled.current) {
       return;
     }
