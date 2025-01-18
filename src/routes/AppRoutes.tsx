dd73638f8625170d56bad41dfe0d8d535a0d13e9
Rahul <DevRaahul>
2025-01-18 20:41:52 +0530

    added lazy loading

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,15 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../components/common/Home";
 import Navbar from "../components/common/Navbar";
-import MemoryBoxGame from "../components/questions/MemoryBox/MemoryBoxGame";
-import PageNotFound from "../components/common/PageNotFound";
-import LoginPage from "../components/common/LoginPage";
-import ToastNotification from "../components/questions/ToastNotification/ToastNotificationContainer";
-import TicTocContainer from "@/components/questions/TicToc/TicTocContainer";
-import FileManagerContainer from "../components/questions/FileManager/FileManagerContainer";
-import ProgressBarContainer from "../components/questions/progressBar/ProgressBarContainer";
+
+const MemoryBoxGame = React.lazy(() => import("../components/questions/MemoryBox/MemoryBoxGame"));
+const PageNotFound = React.lazy(() => import("../components/common/PageNotFound"));
+const LoginPage = React.lazy(() => import("../components/common/LoginPage"));
+const ToastNotification = React.lazy(() => import("../components/questions/ToastNotification/ToastNotificationContainer"));
+const TicTocContainer = React.lazy(() => import("../components/questions/TicToc/TicTocContainer"));
+const FileManagerContainer = React.lazy(() => import("../components/questions/FileManager/FileManagerContainer"));
+const ProgressBarContainer = React.lazy(() => import("../components/questions/progressBar/ProgressBarContainer"));
 
 const AppRoutes = () => {
   return (
