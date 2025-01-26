1891602a04410709b7eccd87868251f1bc5cf898
Rahul <DevRaahul>
2025-01-27 00:32:49 +0530

    ans url added

--- src/constant/questionList.ts
+++ src/constant/questionList.ts
@@ -5,6 +5,7 @@ export const questionList: IQuestion[] = [
     id: 1,
     name: "Memory Sequence Game",
     path: "/sequence_game",
+    codeUrl: "https://github.com/DevRaahul/machine-coding-round/tree/main/src/components/questions/MemoryBox",
     techStack: ["React", "Typescript", "HTML & CSS"],
     description:
       "This is the memory sequence game where we can click boxes in any order and once all boxes are clicked they will unchecked in same/reverse order depending on our program logic.",
@@ -13,6 +14,7 @@ export const questionList: IQuestion[] = [
     id: 2,
     name: "Toast Notification Component",
     path: "/toast",
+    codeUrl: "https://github.com/DevRaahul/machine-coding-round/tree/main/src/components/questions/ToastNotification",
     techStack: ["React", "Typescript", "HTML & CSS"],
     description:
       "In this question we will create our own custom toast notification component. This component will have different varient like 'Success', 'Warning', 'Info' and 'Error'.",
@@ -21,6 +23,7 @@ export const questionList: IQuestion[] = [
     id: 3,
     name: "Tic Tac Toe",
     path: "/tictoc",
+    codeUrl: "https://github.com/DevRaahul/machine-coding-round/tree/main/src/components/questions/TicToc",
     techStack: ["React", "Typescript", "HTML & CSS"],
     description:
       "This is the tic toc game where we can click boxes in any order and once the winning condition is met winner is declared and game is then stops. We can reset the game or undo the recent move.",
@@ -29,6 +32,7 @@ export const questionList: IQuestion[] = [
     id: 4,
     name: "Custom Progress Bar",
     path: "/progress",
+    codeUrl: "https://github.com/DevRaahul/machine-coding-round/tree/main/src/components/questions/progressBar",
     techStack: ["React", "Typescript", "HTML & CSS"],
     description:
       "This is the custom component created for progress bar. Progress bar can be used to show the on going progress of any data fetching action.",
@@ -37,6 +41,7 @@ export const questionList: IQuestion[] = [
     id: 5,
     name: "File Manager",
     path: "/explorer",
+    codeUrl: "https://github.com/DevRaahul/machine-coding-round/tree/main/src/components/questions/FileManager",
     techStack: ["React", "Typescript", "HTML & CSS"],
     description:
       "This is the file explorer component created to show custom file structure. We have used dummy response to mimic the folder structure response.",
@@ -45,6 +50,7 @@ export const questionList: IQuestion[] = [
     id: 6,
     name: "Modal Component",
     path: "/modal",
+    codeUrl: "",
     techStack: ["React", "Typescript", "HTML & CSS"],
     description: "This is the Modal component created to show custom Modals. It has functionality to show/close modal with given  information.",
   },
@@ -52,6 +58,7 @@ export const questionList: IQuestion[] = [
     id: 7,
     name: "FAQ Component",
     path: "/freqAsked",
+    codeUrl: "",
     techStack: ["React", "Typescript", "HTML & CSS"],
     description: "This is the FAQ component created to show FAQ sections. We have used basic HTML structure to show FAQ sections.",
   },
@@ -59,6 +66,7 @@ export const questionList: IQuestion[] = [
     id: 8,
     name: "Nested Commennts",
     path: "/comments",
+    codeUrl: "",
     techStack: ["React", "Typescript", "HTML & CSS"],
     description:
       "This is the Nested comments component created to show multiple comments. We have used basic HTML structure to show comments sections.",
@@ -67,6 +75,7 @@ export const questionList: IQuestion[] = [
     id: 9,
     name: "To-Do List",
     path: "/toDo",
+    codeUrl: "",
     techStack: ["React", "Typescript", "HTML & CSS"],
     description: "This is the basic todo app. We can multiple todo items and changes status of each item.",
   },
@@ -74,6 +83,7 @@ export const questionList: IQuestion[] = [
     id: 10,
     name: "Autocomplete Component",
     path: "/autocomplete",
+    codeUrl: "",
     techStack: ["React", "Typescript", "HTML & CSS"],
     description: "This is the basic autocomplet component. We can search any data like countries/cities and select the value",
   },
@@ -81,6 +91,7 @@ export const questionList: IQuestion[] = [
     id: 11,
     name: "Timer app",
     path: "/stopwatch",
+    codeUrl: "https://github.com/DevRaahul/machine-coding-round/tree/main/src/components/questions/Stopwatch",
     techStack: ["React", "Typescript", "HTML & CSS"],
     description: "This is the basic Timer app. We have implemented features of stopwatch and timer in this app.",
   },
