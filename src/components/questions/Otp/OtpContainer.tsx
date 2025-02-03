1ddb32edc0e3e412acf5c91ee0fdde5b697c2857
Rahul <DevRaahul>
2025-02-04 00:11:32 +0530

    otp changes

--- src/components/questions/Otp/OtpContainer.tsx
+++ src/components/questions/Otp/OtpContainer.tsx
@@ -1,9 +1,24 @@
+import { useRef, useState } from "react";
 import OtpBox from "./OtpBox";
 
 const OtpContainer = () => {
+  const [otp, setOtp] = useState(new Array(6).fill(null));
+  const otpBoxRef = useRef<HTMLInputElement[]>([]);
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>, idx: number) => {
+    let copy = [...otp];
+    copy[idx] = e?.key;
+    console.log(e);
+    console.log(copy);
+    setOtp(copy);
+    if (idx < 5) {
+      otpBoxRef.current[idx + 1].focus();
+    }
+  };
+
   return (
     <>
-      <OtpBox otpFields={6} />
+      <OtpBox otp={otp} handleKeyPress={handleKeyPress} otpBoxRef={otpBoxRef} />
     </>
   );
 };
