4bdd8e69706a230b0eaf7338e16dc30e601a6bac
Rahul <DevRaahul>
2025-02-10 22:33:55 +0530

    added UI changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: false,
+  },
   plugins: [react(), mkcert()],
   server: {
     port: 3000,
