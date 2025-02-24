8815aa36a60b6d854bed3eba2207ec7462e430e9
Rahul <mail.rahulbhoir@gmail.com>
2025-02-25 02:13:35 +0530

    PROGRESS BAR

--- src/components/questions/progressBar/ProgressBar.tsx
+++ src/components/questions/progressBar/ProgressBar.tsx
@@ -3,12 +3,16 @@ interface IProgressBar {
 }
 
 const ProgressBar: React.FC<IProgressBar> = ({ progress }) => {
-  let progressClass = `translate-x-[-${progress}%] `;
-
+  console.log(progress);
   return (
     <>
-      <div className="w-72 h-5 border border-gray-500 rounded-md relative overflow-hidden">
-        <div className={`absolute size-full bg-green-600  text-right`}>inner div</div>
+      <div className="w-72 h-8 border border-gray-500 rounded-md relative overflow-hidden">
+        <div
+          className="flex justify-center items-center absolute size-full bg-green-600  text-right"
+          style={{ transform: `translateX(${progress - 100}%)` }}
+        >
+          {`${progress}%`}
+        </div>
       </div>
     </>
   );
--- src/components/questions/progressBar/ProgressBarContainer.tsx
+++ src/components/questions/progressBar/ProgressBarContainer.tsx
@@ -1,10 +1,20 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import ProgressBar from "./ProgressBar";
 
 const ProgressBarContainer: FC = () => {
+  const [loadTime, setTime] = useState<number>(0);
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setTime((prev) => (prev <= 99 ? prev + 1 : 100));
+    }, 500);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <>
-      <ProgressBar progress={50} />
+      <ProgressBar progress={loadTime} />
     </>
   );
 };
