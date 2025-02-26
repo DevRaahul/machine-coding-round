e632f08bf04f94291625ef3277b615cdc364796e
Rahul <mail.rahulbhoir@gmail.com>
2025-02-26 23:56:27 +0530

    PROGRESS BAR

--- src/components/questions/progressBar/ProgressBar.tsx
+++ src/components/questions/progressBar/ProgressBar.tsx
@@ -8,10 +8,10 @@ const ProgressBar: React.FC<IProgressBar> = ({ progress }) => {
     <>
       <div className="w-72 h-8 border border-gray-500 rounded-md relative overflow-hidden">
         <div
-          className="flex justify-center items-center absolute size-full bg-green-600  text-right"
+          className="flex justify-st items-center absolute size-full bg-green-600  text-right"
           style={{ transform: `translateX(${progress - 100}%)` }}
         >
-          {`${progress}%`}
+          {`${progress}%`}vjhvjhjg
         </div>
       </div>
     </>
