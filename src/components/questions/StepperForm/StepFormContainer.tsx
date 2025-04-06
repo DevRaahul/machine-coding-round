a74cd38ed7d8b792ce8dd0a18d5c1b6deab66ad2
Rahul <mail.rahulbhoir@gmail.com>
2025-04-06 23:57:36 +0530

    Added btn

--- src/components/questions/StepperForm/StepFormContainer.tsx
+++ src/components/questions/StepperForm/StepFormContainer.tsx
@@ -1,14 +1,23 @@
+import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import FormOne from "./FormOne";
 import FormTwo from "./FormTwo";
 
 const StepFormContainer = () => {
   const [step, setStep] = useState<number>(1);
+
+  const backButton = <Button onClick={() => setStep(step - 1)}>Back</Button>;
+  const forwardButton = <Button onClick={() => setStep(step + 1)}>{step === 2 ? "Submit" : "Next"}</Button>;
+
   return (
-    <>
-      {step === 1 && <FormOne />}
-      {step === 2 && <FormTwo />}
-    </>
+    <div>
+      <div>
+        {step === 1 && <FormOne />}
+        {step === 2 && <FormTwo />}
+        {backButton}
+        {forwardButton}
+      </div>
+    </div>
   );
 };
 
